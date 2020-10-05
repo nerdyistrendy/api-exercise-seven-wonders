5c671c974352c3515911e3dacd59cc7ace33d807
Li Dai <li.lea.dai@gmail.com>
2020-10-05 13:29:15 -0700

    hid the API key with dotenv

--- seven_wonders.rb
+++ seven_wonders.rb
@@ -1,8 +1,12 @@
 require 'httparty'
 require "awesome_print"
+require 'dotenv'
+require "prettyprint"
+
+Dotenv.load
 
 BASE_URL = "https://us1.locationiq.com/v1/search.php"
-LOCATION_IQ_KEY = "3a04b55f0bc2c1"
+LOCATION_IQ_KEY = ENV["key"]
 
 def get_location(search_term)
   query= {
@@ -70,7 +74,7 @@ ap find_seven_wonders
 # [{"Great Pyramid of Giza"=>{:lat=>"29.9791264", :lon=>"31.1342383751015"}}, {"Gardens of Babylon"=>{:lat=>"50.8241215", :lon=>"-0.1506162"}}, {"Colossus of Rhodes"=>{:lat=>"36.3397076", :lon=>"28.2003164"}}, {"Pharos of Alexandria"=>{:lat=>"30.94795585", :lon=>"29.5235626430011"}}, {"Statue of Zeus at Olympia"=>{:lat=>"37.6379088", :lon=>"21.6300063"}}, {"Temple of Artemis"=>{:lat=>"32.2818952", :lon=>"35.8908989553238"}}, {"Mausoleum at Halicarnassus"=>{:lat=>"37.03788265", :lon=>"27.4241455276707"}}]
 
 coordinates = get_coordinates("Cairo Egypt", "Great Pyramid of Giza")
-puts driving_directions(coordinates)
+pp driving_directions(coordinates)
 
 locations = [{ lat: 38.8976998, lon: -77.0365534886228}, {lat: 48.4283182, lon: -123.3649533 }, { lat: 41.8902614, lon: 12.493087103595503}]
 ap coordinates_to_name(locations)
