f90c26960798088c0d5ebea1a09add4bcffa68a6
Li Dai <li.lea.dai@gmail.com>
2020-10-05 11:14:19 -0700

    added methods get_coordinates and driving_directions

--- seven_wonders.rb
+++ seven_wonders.rb
@@ -1,7 +1,7 @@
 require 'httparty'
 require "awesome_print"
 
-BASE_URL = "https://us1.locationiq.com/v1/search.php?"
+BASE_URL = "https://us1.locationiq.com/v1/search.php"
 LOCATION_IQ_KEY = "3a04b55f0bc2c1"
 
 def get_location(search_term)
@@ -11,9 +11,9 @@ def get_location(search_term)
       format: "json"
   }
   response = HTTParty.get(BASE_URL, query: query)
-  location = {search_term => {:lat => response[0]["lat"], :lon => response[0]["lon"]}}
+  location = {search_term => {:lat => response[0]["lat"],
+                              :lon => response[0]["lon"]}}
   return location
-
 end
 
 def find_seven_wonders
@@ -30,9 +30,22 @@ def find_seven_wonders
   return seven_wonders_locations
 end
 
-get_location("Great Pyramid of Giza")
+def driving_directions(coordinates)
+  url = "https://us1.locationiq.com/v1/directions/driving/#{coordinates}"
+  query= {key: LOCATION_IQ_KEY}
+  p response = HTTParty.get(url, query: query)
+end
+
+def get_coordinates(search1, search2)
+  l1 = get_location(search1)[search1]
+  l2 = get_location(search2)[search2]
+  coordinates = "#{l1[:lat]},#{l1[:lon]};#{l2[:lat]},#{l1[:lon]}"
+  return coordinates
+end
 
 # Use awesome_print because it can format the output nicely
-ap find_seven_wonders
+# ap find_seven_wonders
 # Expecting something like:
 # [{"Great Pyramid of Giza"=>{:lat=>"29.9791264", :lon=>"31.1342383751015"}}, {"Gardens of Babylon"=>{:lat=>"50.8241215", :lon=>"-0.1506162"}}, {"Colossus of Rhodes"=>{:lat=>"36.3397076", :lon=>"28.2003164"}}, {"Pharos of Alexandria"=>{:lat=>"30.94795585", :lon=>"29.5235626430011"}}, {"Statue of Zeus at Olympia"=>{:lat=>"37.6379088", :lon=>"21.6300063"}}, {"Temple of Artemis"=>{:lat=>"32.2818952", :lon=>"35.8908989553238"}}, {"Mausoleum at Halicarnassus"=>{:lat=>"37.03788265", :lon=>"27.4241455276707"}}]
+coordinates = get_coordinates("Cairo Egypt", "Great Pyramid of Giza")
+ap driving_directions(coordinates)
